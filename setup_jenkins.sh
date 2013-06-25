3cbb0457e1895ee7216440f5c2fd6e96a5579f78
Steven L. Speek <slspeek@gmail.com>
2013-06-25 20:21:05 +0200

    Trying to checkout fspotcloud.jenkins in ~jenkins

--- setup_jenkins.sh
+++ setup_jenkins.sh
@@ -2,8 +2,11 @@ wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key 
 echo deb http://pkg.jenkins-ci.org/debian binary/>>/etc/apt/sources.list
 sudo apt-get --yes --force-yes update
 sudo apt-get --yes --force-yes install jenkins
+
+/etc/init.d/jenkins stop
 sed -i -e 's^/var/lib/jenkins^/home/jenkins^' /etc/passwd /etc/default/jenkins
 mkdir /home/jenkins
 chown -R jenkins /home/jenkins
+sudo -u jenkins hg clone https://code.google.com/p/fspotcloud.jenkins/ .
 sed -i -e 's/8080/9000/' /etc/default/jenkins
 /etc/init.d/jenkins restart
