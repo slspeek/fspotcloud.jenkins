137c8633fc594434f1762151ad9c201e25df9608
Steven L. Speek <slspeek@gmail.com>
2015-04-20 20:28:24 +0200

    Simple_JPA_DAO builds

--- install_jenkins_plugins.sh
+++ install_jenkins_plugins.sh
@@ -1,7 +1,7 @@
 JENKINS_URL=http://localhost:9000
 wget $JENKINS_URL/jnlpJars/jenkins-cli.jar
 INSTALL_PLUGIN="java -jar jenkins-cli.jar -s $JENKINS_URL install-plugin"
-for PLUGIN in copy-to-slave checkstyle cobertura findbugs jdepend pmd gradle mercurial depgraph-view htmlpublisher clone-workspace-scm;
+for PLUGIN in git copy-to-slave checkstyle cobertura findbugs jdepend pmd gradle mercurial depgraph-view htmlpublisher clone-workspace-scm;
 do
 $INSTALL_PLUGIN $PLUGIN
 done
